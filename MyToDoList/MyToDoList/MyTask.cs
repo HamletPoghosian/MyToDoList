10ac2a2bb739e90defd9cd1890606a9bca4c8683
Hamlet Poghosian <phamik@mail.ru>
2019-04-01 15:45:11 +0400

    Creat new Lib

---
+++ MyToDoList/MyToDoList/MyTask.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDoList
+{
+	public class MyTask
+	{
+		private string mywork;
+		private bool isdone;
+		private DateTime deadline;
+
+		public MyTask()
+		{
+
+		}
+		public MyTask(string s, bool b, DateTime d)
+		{
+			Mywork = s;
+			Isdone = b;
+			Deadline = d;
+		}
+		public string Mywork
+		{
+			get => mywork;
+			set => mywork = value;
+		}
+		public bool Isdone
+		{
+			get => isdone;
+			set => isdone = value;
+		}
+		public DateTime Deadline
+		{
+			get => deadline;
+
+			set
+			{
+				if (value > DateTime.MinValue && value < DateTime.MaxValue)
+				{
+					deadline = value;
+				}
+				else
+				{
+					throw new Exception("Data is invalid");
+				}
+			}
+		}
+		public override string ToString()
+		{
+			return Mywork + "\t" + Isdone + "\t" + Deadline;
+		}
+	}
+}
---
+++ MyToDoList/MyToDoList/MyToDoList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDoList
+{
+	public class MyToDoList : IEnumerable<MyTask>
+	{
+		private List<MyTask> list = new List<MyTask>();
+
+
+		public void Add(MyTask mytask)
+		{
+			list.Add(mytask);
+
+		}
+		public void Del(MyTask mytask)
+		{
+			list.Remove(mytask);
+		}
+		public void AddtoIndex(MyTask mytask, int index)
+		{
+			if (index > list.Count)
+			{
+				throw new IndexOutOfRangeException();
+			}
+			else
+			{
+				list.Insert(index, mytask);
+			}
+		}
+		public IEnumerator GetEnumerator()
+		{
+			for (int i = 0; i < list.Count; i++)
+			{
+				yield return list[i];
+			}
+		}
+		public List<MyTask> ShowCompletit(MyToDoList todo)
+		{
+			return todo.Where(x => x.Isdone == true).ToList();
+
+
+		}
+		IEnumerator<MyTask> IEnumerable<MyTask>.GetEnumerator()
+		{
+			return list.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
